785500ec6b4d1134990f72bcab642fbf5a925bad
Brent Bailey <bb2906@nyu.edu>
2019-02-12 18:13:41 -0500

    download functioning

--- download.js
+++ download.js
@@ -1,7 +1,9 @@
 console.log('download script appended');
 function downloadEntry(event) {
-  event.preventDefault();
+  console.log(event);
+  event.stopPropagation();
   console.log('donwloading entry')
+  event.preventDefault();
   let dateOb = new Date();
   let date = dateOb.getDate();
   let month = dateOb.getMonth();
@@ -10,7 +12,8 @@ function downloadEntry(event) {
   let dateString = date + "_" + (month + 1) + "_" + year;
 
   let inputs = event.target.getElementsByTagName("input");
-  let content = `${dateString}: \n Feelings:\n ${inputs[0]}\n Why:\n ${inputs[1]}\n Conclusion:\n ${inputs[2]}`;
+  console.log('inputs: ', inputs)
+  let content = `${dateString}: \n Feelings:\n ${inputs[0].value}\n Why:\n ${inputs[1].value}\n Conclusion:\n ${inputs[2].value}`;
   window.open("data:application/txt," + encodeURIComponent(content), "_self");
 }
 
