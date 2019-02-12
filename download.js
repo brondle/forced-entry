eced08018e22df5d78f0bfd99e17a75a3424cec4
Brent Bailey <bb2906@nyu.edu>
2019-02-12 18:19:59 -0500

    allow to download with datetime/set title

--- download.js
+++ download.js
@@ -7,13 +7,30 @@ function downloadEntry(event) {
   let dateOb = new Date();
   let date = dateOb.getDate();
   let month = dateOb.getMonth();
-  let year = dateOb.getYear();
+  let year = dateOb.getFullYear();
 
   let dateString = date + "_" + (month + 1) + "_" + year;
 
   let inputs = event.target.getElementsByTagName("input");
   console.log('inputs: ', inputs)
   let content = `${dateString}: \n Feelings:\n ${inputs[0].value}\n Why:\n ${inputs[1].value}\n Conclusion:\n ${inputs[2].value}`;
-  window.open("data:application/txt," + encodeURIComponent(content), "_self");
+  formatAndSave(`journal_entry_${dateString}`, content);
+}
+
+
+function formatAndSave(filename, text) {
+  // indebted to https://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file for this solution
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+
+  if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
 }
 
